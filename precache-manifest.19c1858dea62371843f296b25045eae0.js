365fb92f44f079d9ee2baa4b375cfbe02ac10b22
Leonardo Cruz <cruzleonardo00@gmail.com>
2020-07-09 14:03:29 -0500

    Updates

---
+++ precache-manifest.19c1858dea62371843f296b25045eae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c53af09437baa17106607889f5a9a2",
+    "url": "/github/io/Curriculum/index.html"
+  },
+  {
+    "revision": "358084adf5d2df377172",
+    "url": "/github/io/Curriculum/static/css/main.ca31fb96.chunk.css"
+  },
+  {
+    "revision": "45ea64226e7b91c49ae9",
+    "url": "/github/io/Curriculum/static/js/2.b6906019.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github/io/Curriculum/static/js/2.b6906019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358084adf5d2df377172",
+    "url": "/github/io/Curriculum/static/js/main.de912992.chunk.js"
+  },
+  {
+    "revision": "198468620b50dccef5db",
+    "url": "/github/io/Curriculum/static/js/runtime-main.d97d72bd.js"
+  }
+]);
